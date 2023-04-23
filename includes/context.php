63e40e3b840c079df2fac5cbee1bc5d60ded535d
IRPCPRO <designer.pcpro@yahoo.com>
2023-04-23 11:36:22 +0430

    move includes

--- includes/context.php
+++ includes/context.php
@@ -19,22 +19,22 @@
             <div class="bg-icons d-flex-row-left">
                 <a href="<?= SOCIAL_GITHUB ?>" title="github" target="_blank">
                     <li class="icon icon-github">
-                        <img src="./assets/images/icon-github.svg" alt="github">
+                        <img src="../assets/images/icon-github.svg" alt="github">
                     </li>
                 </a>
                 <a href="<?= SOCIAL_LINKEDIN ?>" title="linkedin" target="_blank">
                     <li class="icon icon-linkedin">
-                        <img src="./assets/images/icon-linkedin.svg" alt="linkedin">
+                        <img src="../assets/images/icon-linkedin.svg" alt="linkedin">
                     </li>
                 </a>
                 <a href="<?= SOCIAL_YOUTUBE ?>" title="youtube" target="_blank">
                     <li class="icon icon-youtube">
-                        <img src="./assets/images/icon-youtube.svg" alt="youtube">
+                        <img src="../assets/images/icon-youtube.svg" alt="youtube">
                     </li>
                 </a>
                 <a href="mailto: <?= SOCIAL_EMAIL ?>" title="email" target="_blank">
                     <li class="icon icon-email">
-                        <img src="./assets/images/icon-email.svg" alt="email">
+                        <img src="../assets/images/icon-email.svg" alt="email">
                     </li>
                 </a>
             </div>
@@ -42,6 +42,6 @@
     </div>
 
     <div class="right">
-        <img class="image-face" src="./assets/images/image-face.png" alt="face" />
+        <img class="image-face" src="../assets/images/image-face.png" alt="face" />
     </div>
 </section>
--- includes/header.php
+++ includes/header.php
@@ -2,13 +2,13 @@
     <div class="wrapper">
         <div class="header-bg d-flex-row-space-between">
             <div class="left">
-                <img class="header-logo-irpcpro" src="./assets/images/icon-irpcpro.svg" alt="logo" />
+                <img class="header-logo-irpcpro" src="../assets/images/icon-irpcpro.svg" alt="logo" />
                 <div class="header-logo-text font-dana">
                     Known as <span>IRPCPRO</span>
                 </div>
             </div>
             <div class="right">
-                <img class="header-resume-icon" src="./assets/images/icon-resume.svg" alt="resume" />
+                <img class="header-resume-icon" src="../assets/images/icon-resume.svg" alt="resume" />
                 <div class="header-resume-text font-dana">
                     Dl Resume
                 </div>
